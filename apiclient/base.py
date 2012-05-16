b5b6005944ddce1b9ca4c488375d8b988da04dba
George Marshall <echosx@gmail.com>
2012-05-16 10:32:37 -0700

    Changed to use urllib3 url encoding methods

--- apiclient/base.py
+++ apiclient/base.py
@@ -14,36 +14,35 @@ class APIClient(object):
     def _make_connection_pool(self, url):
         return connection_from_url(url)
 
-    def _compose_url(self, path, params=None):
-        return self.BASE_URL + path + '?' + urlencode(params)
+    def _compose_url(self, method, url, fields=None):
+        return (method, self.BASE_URL + path, fields)
 
     def _handle_response(self, response):
         return json.loads(response.data)
 
-    def _request(self, method, path, params=None):
-        url = self._compose_url(path, params)
-
+    def _request(self, method, path, fields=None):
         self.rate_limit_lock and self.rate_limit_lock.acquire()
-        r = self.connection_pool.urlopen(method.upper(), url)
+        r = self.connection_pool.request(*self._compose_url(method, path, fields))
 
         return self._handle_response(r)
 
-    def call(self, path, **params):
-        return self._request('GET', path, params=params)
+    def call(self, path, **fields):
+        return self._request('GET', path, fields=fields)
 
 
 class APIClient_SharedSecret(APIClient):
     API_KEY_PARAM = 'key'
 
-    def __init__(self, api_key, *args, **kw):
+    def __init__(self, api_key=None, *args, **kw):
         super(APIClient_SharedSecret, self).__init__(*args, **kw)
         self.api_key = api_key
 
-    def _compose_url(self, path, params=None):
-        # TODO: fix this, as per our conversation at Oct. 4, 2011, 05:10 UTC
-        p = {self.API_KEY_PARAM: self.api_key}
+    def _compose_url(self, method, path, fields=None):
+        p = {}
+        if self.api_key is not None:
+            p[self.API_KEY_PARAM] = self.api_key
 
-        if params:
-            p.update(params)
+        if fields:
+            p.update(fields)
 
-        return self.BASE_URL + path + '?' + urlencode(p)
+        return (method, self.BASE_URL + path, p)
